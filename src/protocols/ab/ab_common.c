f6638d5da62dacb2630f7c2f6e598abe3aa4b994
Phil Smith <pts@observatorysciences.co.uk>
2024-04-08 16:50:48 +0100

    Removed limitation which stopped users setting elem_count for omronNJ PLCs

--- src/protocols/ab/ab_common.c
+++ src/protocols/ab/ab_common.c
@@ -440,14 +440,8 @@ plc_tag_p ab_tag_create(attr attribs, void (*tag_callback_func)(int32_t tag_id, 
     tag->elem_count = attr_get_int(attribs,"elem_count", 1);
 
     switch(tag->plc_type) {
+        /* fall through */
     case AB_PLC_OMRON_NJNX:
-        if (tag->elem_count != 1) {
-            tag->elem_count = 1;
-            pdebug(DEBUG_WARN,"Attribute elem_count should be 1!");
-        }
-
-        /* from here is the same as a AB_PLC_MICRO800. */
-
         /* fall through */
     case AB_PLC_LGX:
     case AB_PLC_MICRO800:
